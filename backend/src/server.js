31dcdd8a7c6e1376c36a3654cfc269d72ab24e5a
VladimirKomov <v.e.komov@gmail.com>
2024-12-01 12:49:55 +0200

    Initialize project with React frontend and Express backend

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
